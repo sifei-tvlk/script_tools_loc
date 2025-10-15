03b2a2fb931ae4423a4b52b88f882bf62e1661e5
sifei-tvlk <sifei.li@traveloka.com>
2025-10-15 17:32:06 +0800

    fix

--- naver/get_loc_db.py
+++ naver/get_loc_db.py
@@ -1,4 +1,4 @@
-from ../LocgiApi import get_geo_with_geometry_by_id, get_children_geo_by_id
+from ../LocgiApi import GeoDataService
 from UserUtils import get_locgi_url
 from llm_tool import get_llm_response
 
@@ -11,10 +11,10 @@ with open(hier_dict_test_file, 'r') as file:
 
 sk_id = 20004311
 
-l1_ids = get_children_geo_by_id(sk_id, locgi_url)
+l1_ids = GeoDataService.get_children_geo_by_id(sk_id, locgi_url)
 
 for l1_id in l1_ids:
-    result = get_geo_with_geometry_by_id(l1_id, locgi_url)
+    result = GeoDataService.get_geo_with_geometry_by_id(l1_id, locgi_url)
     print(result)
 
 # level 1 name checking
