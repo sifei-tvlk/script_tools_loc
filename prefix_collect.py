62aa73b622899de951008553cf15e468e76c8027
sifei-tvlk <sifei.li@traveloka.com>
2025-10-24 12:03:30 +0700

    fix

--- prefix_collect.py
+++ prefix_collect.py
@@ -4,10 +4,7 @@ from UserUtils import UserInput
 
 result = []
 
-geo_id_jp = 20001756
-geo_id_kr = 20004311
-geo_id_vi = 10009958
-geo_id_th = 10000007
+geo_id_asia = 30019581
 
 prefix_thai = ["อำเภอ",
                "ตำบล",
@@ -33,6 +30,7 @@ suffix_kr = [
 suffix_jp = [
     "県",
     "市",
+    "郡",
     "町",
     "島",
     "区"
@@ -40,58 +38,58 @@ suffix_jp = [
 
 modify_dict = {
     'jp': {
-        'id': geo_id_jp,
+        'id': geo_id_asia,
         'locale': 'ja_jp',
         "type": 'suffix',
         "suffix": suffix_jp
     },
     'kr': {
-        'id': geo_id_kr,
+        'id': geo_id_asia,
         'locale': 'ko_ko',
         "type": 'suffix',
         "suffix": suffix_kr
     },
     'vi': {
-        'id': geo_id_vi,
+        'id': geo_id_asia,
         'locale': 'vi_vn',
         "type": 'prefix',
         "prefix": prefix_viet
     },
     'th': {
-        'id': geo_id_th,
+        'id': geo_id_asia,
         'locale': 'th_th',
         "type": 'prefix',
         "prefix": prefix_thai
     }
 }
 
-def fetch_children(parent_geo_id, country_code, locgi_url):
+def fetch_children(parent_geo_id, language, locgi_url):
     geo_regions = GeoDataService.get_children_geo_by_id(parent_geo_id, locgi_url)
     if not geo_regions:
         return
     for region in geo_regions:
         geo_id = region.get('geoId')
         name = region.get('name')
-        res = GeoDataService.get_geo_theme(geo_id, modify_dict[country_code]['locale'], locgi_url)
+        country_code = region.get('countryCode')
+        res = GeoDataService.get_geo_theme(geo_id, modify_dict[language]['locale'], locgi_url)
         if not res:
-            # print(f"NO THEME for {country_code} geoId {geo_id} name {name}")
             continue
         local_name = res.get('localName', '')
-        if modify_dict[country_code]['type'] == 'prefix':
-            for prefix in modify_dict[country_code]['prefix']:
+        if modify_dict[language]['type'] == 'prefix':
+            for prefix in modify_dict[language]['prefix']:
                 if local_name.startswith(prefix):
                     trimmed_name = local_name[len(prefix):].strip()
-                    result.append([country_code, geo_id, name, local_name, prefix, trimmed_name])
+                    result.append([language, country_code, geo_id, name, local_name, trimmed_name])
                     print(f"geoId {geo_id} name {local_name}")
                     break
-        elif modify_dict[country_code]['type'] == 'suffix':
-            for suffix in modify_dict[country_code]['suffix']:
+        elif modify_dict[language]['type'] == 'suffix':
+            for suffix in modify_dict[language]['suffix']:
                 if local_name.endswith(suffix):
                     trimmed_name = local_name[:-len(suffix)].strip()
-                    result.append([country_code, geo_id, name, local_name, suffix, trimmed_name])
+                    result.append([language, country_code, geo_id, name, local_name, trimmed_name])
                     print(f"geoId {geo_id} name {local_name}")
                     break
-        fetch_children(region.get('geoId'), country_code, locgi_url)
+        fetch_children(region.get('geoId'), language, locgi_url)
 
 def main():
     env = UserInput.choose_env()
@@ -100,12 +98,12 @@ def main():
         geo_id = modify_dict[country_code]['id']
         fetch_children(geo_id, country_code, locgi_url)
 
-    with open('suffix-prefix_check.csv', 'w', newline='') as csvfile:
-        spamwriter = csv.writer(csvfile, delimiter=',',
-                                quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        spamwriter.writerow(['country-code', 'geoId', 'name', 'local-name', 'prefix', 'trimmed-name'])
-        for row in result:
-            spamwriter.writerow(row)
+        with open(f'{modify_dict[country_code]["type"]}_check_{modify_dict[country_code]["locale"]}.csv', 'w', newline='') as csvfile:
+            spamwriter = csv.writer(csvfile, delimiter=',',
+                                    quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            spamwriter.writerow(['country-code', 'geoId', 'name', 'local-name', 'prefix', 'trimmed-name'])
+            for row in result:
+                spamwriter.writerow(row)
 
 if __name__ == "__main__":
     main()
