b30d19094c22600d1867ee2cc11c79c0f104ae16
sifei-tvlk <sifei.li@traveloka.com>
2025-10-29 13:54:30 +0800

    fix

--- bracket_check.py
+++ bracket_check.py
@@ -25,19 +25,25 @@ modify_dict = {
 
 def fetch_children(parent_geo_id, language, locgi_url):
     result = []
+    theme_res = GeoDataService.get_geo_theme(parent_geo_id, modify_dict[language]['locale'], locgi_url)
+    name = theme_res.get('name')
+    country_code = theme_res.get('countryISO')
+    if theme_res:
+        local_name = res.get('localName', '')
+        if local_name.find('(') != -1 or local_name.find(')') != -1 or local_name.find('（') != -1 or local_name.find('）') != -1: 
+            # print(f"id: {geo_id}, name {name}, local name {local_name}")
+            result.append([language, country_code, geo_id, name, local_name])
     geo_regions = GeoDataService.get_children_geo_by_id(parent_geo_id, locgi_url)
-    if not geo_regions:
-        return []
     for region in geo_regions:
-        geo_id = region.get('geoId')
-        name = region.get('name')
-        country_code = region.get('countryISO')
-        res = GeoDataService.get_geo_theme(geo_id, modify_dict[language]['locale'], locgi_url)
-        if res:
-            local_name = res.get('localName', '')
-            if local_name.find('(') != -1 or local_name.find(')') != -1 or local_name.find('（') != -1 or local_name.find('）') != -1: 
-                # print(f"id: {geo_id}, name {name}, local name {local_name}")
-                result.append([language, country_code, geo_id, name, local_name])
+        # geo_id = region.get('geoId')
+        # name = region.get('name')
+        # country_code = region.get('countryISO')
+        # res = GeoDataService.get_geo_theme(geo_id, modify_dict[language]['locale'], locgi_url)
+        # if res:
+        #     local_name = res.get('localName', '')
+        #     if local_name.find('(') != -1 or local_name.find(')') != -1 or local_name.find('（') != -1 or local_name.find('）') != -1: 
+        #         # print(f"id: {geo_id}, name {name}, local name {local_name}")
+        #         result.append([language, country_code, geo_id, name, local_name])
         sub_result = fetch_children(region.get('geoId'), language, locgi_url)
     if sub_result:
         result.extend(sub_result)
@@ -61,7 +67,7 @@ def main():
                 name = country.get('name')
                 country_id = country.get('geoId')
                 country_code = country.get('countryISO')
-                result = fetch_children(continent_id, language, locgi_url)
+                result = fetch_children(country_id, language, locgi_url)
                 if not result:
                     continue
                 with open(f"./bracket_check/bracket_check_{country_code}_{language}.csv", 'w', newline='') as csvfile:
