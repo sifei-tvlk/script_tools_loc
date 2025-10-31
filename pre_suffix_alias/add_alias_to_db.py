0e547b2d99ecd1e0f5e3caee95070f7a60618699
sifei-tvlk <sifei.li@traveloka.com>
2025-10-31 17:10:21 +0800

    update

--- pre_suffix_alias/add_alias_to_db.py
+++ pre_suffix_alias/add_alias_to_db.py
@@ -1,4 +1,6 @@
 import csv
+import os
+import sys
 
 parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.insert(0, parent_dir)
@@ -14,18 +16,20 @@ with open('bracket_check_jp_alias_fixed.csv', mode='r', encoding='utf-8') as fil
     reader = csv.DictReader(file)
     for row in reader:
         geoId = row['geoId']
-        alias = row['alias']
+        alias = row['alias'].split(",")
+        for i, a in enumerate(alias):
+            alias[i] = a.strip(" ").strip("[]'\"")
         if geoId and alias:
-            geo_alias_map[geoId] = [a.strip() for a in alias.split(',') if a.strip()]
-print(geo_alias_map)
+            geo_alias_map[geoId] = alias
+            print(geoId, alias)
 
 # Step 2: Loop through the dictionary and update via API
 for geoId, new_aliases in geo_alias_map.items():
     data = GeoDataService.get_geo_with_geometry_by_id(geoId, locgi_url)
-    print(data)
     existing_aliases = data.get('alias', [])
     combined_aliases = list(set(existing_aliases + new_aliases))  # Avoid duplicates
     data['alias'] = combined_aliases
-    print(data)
-    # GeoDataService.upsert_geo_with_geometry(data, locgi_url)
+    GeoDataService.upsert_geo_with_geometry(data, locgi_url)
+    new_data = GeoDataService.get_geo_with_geometry_by_id(geoId, locgi_url)
+    print(geoId, new_data.get('alias', []))
     break
